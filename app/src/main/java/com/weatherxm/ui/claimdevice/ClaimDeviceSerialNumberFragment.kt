fd017feda2c1ea4878bf4f871de7f41c425c9454
Stratos Theodorou <eeVoskos@gmail.com>
2022-02-09 11:54:57 +0200

    Mask serial number typed input (#28)
    * Added colon ":" separator when typing the serial number.
    
    * Delete colon character on keyboard delete.
    
    * Convert serial number typed characters to uppercase

--- app/src/main/java/com/weatherxm/ui/claimdevice/ClaimDeviceSerialNumberFragment.kt
+++ app/src/main/java/com/weatherxm/ui/claimdevice/ClaimDeviceSerialNumberFragment.kt
@@ -1,16 +1,19 @@
 package com.weatherxm.ui.claimdevice
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.InputFilter
+import android.text.InputType
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import com.redmadrobot.inputmask.MaskedTextChangedListener
 import com.weatherxm.R
 import com.weatherxm.databinding.FragmentClaimDeviceBySerialBinding
 import com.weatherxm.util.Validator
-import com.weatherxm.util.onTextChanged
 import com.weatherxm.util.setHtml
 import org.koin.android.ext.android.inject
 
@@ -28,10 +31,27 @@ class ClaimDeviceSerialNumberFragment : Fragment() {
 
         binding.instructions.setHtml(R.string.device_serial_number_instructions)
 
-        binding.serialNumber.onTextChanged { serial ->
-            binding.serialNumberContainer.error = null
-            binding.next.isEnabled = serial.isNotEmpty()
-        }
+        // Convert typed characters to uppercase
+        binding.serialNumber.filters = arrayOf(InputFilter.AllCaps())
+
+        // Mask serial number input
+        MaskedTextChangedListener.installOn(
+            editText = binding.serialNumber,
+            primaryFormat = SERIAL_NUMBER_MASK,
+            autocomplete = false,
+            autoskip = true,
+            valueListener = object : MaskedTextChangedListener.ValueListener {
+                override fun onTextChanged(
+                    maskFilled: Boolean,
+                    extractedValue: String,
+                    formattedValue: String
+                ) {
+                    binding.serialNumberContainer.error = null
+                    binding.serialNumberContainer.helperText =
+                        "${extractedValue.length}/$SERIAL_NUMBER_MAX_LENGTH"
+                    binding.next.isEnabled = extractedValue.isNotEmpty()
+                }
+            })
 
         binding.serialNumber.setOnEditorActionListener { _, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_NEXT) {
@@ -41,7 +61,7 @@ class ClaimDeviceSerialNumberFragment : Fragment() {
         }
 
         binding.next.setOnClickListener {
-            val serialNumber = binding.serialNumber.text.toString()
+            val serialNumber = binding.serialNumber.text.unmask()
 
             if (!validator.validateSerialNumber(serialNumber)) {
                 binding.serialNumberContainer.error = getString(R.string.invalid_serial_number)
@@ -53,5 +73,15 @@ class ClaimDeviceSerialNumberFragment : Fragment() {
         }
 
         return binding.root
+    }
+
+    /**
+     * Remove colon ":" character from Serial Number text
+     */
+    private fun Editable?.unmask(): String = this.toString().replace(":", "")
+
+    companion object {
+        const val SERIAL_NUMBER_MASK = "[__]:[__]:[__]:[__]:[__]:[__]:[__]:[__]:[__]"
+        const val SERIAL_NUMBER_MAX_LENGTH = 18
     }
 }
