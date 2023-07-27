ad6b83e8ebeee92021170b05fb4fe8f85ee852ab
Pavlos Tzegiannakis <pavlostze@gmail.com>
2023-07-27 19:51:22 +0300

    Update AGP (#318)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") version "8.0.2" apply false
+    id("com.android.application") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.google.devtools.ksp") version "1.9.0-1.0.12" apply false
     id("com.google.gms.google-services") version "4.3.15" apply false
