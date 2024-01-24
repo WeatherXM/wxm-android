5255ce13d6558ed54be6765fc37506e9d0cb7df9
Pavlos Tzegiannakis <pavlostzeg@gmail.com>
2024-01-24 13:48:42 +0200

    Fix detekt

--- app/src/main/java/com/weatherxm/ui/components/BaseMapFragment.kt
+++ app/src/main/java/com/weatherxm/ui/components/BaseMapFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import com.mapbox.geojson.Point
 import com.mapbox.maps.CameraOptions
 import com.mapbox.maps.MapView
