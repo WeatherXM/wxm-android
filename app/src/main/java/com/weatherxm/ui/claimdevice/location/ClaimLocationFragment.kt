d09420564a43094cdcdc22d87cd621bf074a1a3c
Pavlos Tzegiannakis <pavlostzeg@gmail.com>
2024-01-29 14:41:32 +0200

    Fix detekt

--- app/src/main/java/com/weatherxm/ui/claimdevice/location/ClaimLocationFragment.kt
+++ app/src/main/java/com/weatherxm/ui/claimdevice/location/ClaimLocationFragment.kt
@@ -1,7 +1,6 @@
 package com.weatherxm.ui.claimdevice.location
 
 import android.annotation.SuppressLint
-import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
