fdc557c5de32194a143761a59419707c1d572ea4
Pavlos Tzegiannakis <pavlostzeg@gmail.com>
2024-06-04 17:54:34 +0300

    FE-889 Feature/optimize change frequency code (#181)
    * Create a Helium Set Frequency View and integrate it into Claiming step
    
    * Integrate the new component in ChangeFrequencyActivity

--- app/src/main/java/com/weatherxm/ui/claimdevice/helium/frequency/ClaimHeliumFrequencyFragment.kt
+++ app/src/main/java/com/weatherxm/ui/claimdevice/helium/frequency/ClaimHeliumFrequencyFragment.kt
@@ -4,12 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import com.weatherxm.R
 import com.weatherxm.analytics.AnalyticsService
 import com.weatherxm.databinding.FragmentClaimHeliumFrequencyBinding
 import com.weatherxm.ui.claimdevice.helium.ClaimHeliumViewModel
-import com.weatherxm.ui.common.setHtml
 import com.weatherxm.ui.components.BaseFragment
 import org.koin.androidx.viewmodel.ext.android.activityViewModel
 
@@ -30,47 +27,25 @@ class ClaimHeliumFrequencyFragment : BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        with(binding.description) {
-            movementMethod =
-                me.saket.bettermovementmethod.BetterLinkMovementMethod.newInstance().apply {
-                    setOnLinkClickListener { _, url ->
-                        analytics.trackEventSelectContent(
-                            AnalyticsService.ParamValue.DOCUMENTATION_FREQUENCY.paramValue
-                        )
-                        navigator.openWebsite(context, url)
-                        return@setOnLinkClickListener true
-                    }
-                }
-            setHtml(R.string.set_frequency_desc, getString(R.string.helium_frequencies_mapping_url))
-        }
-
-        binding.confirmFrequencyToggle.setOnCheckedChangeListener { _, checked ->
-            binding.setAndClaimButton.isEnabled = checked
-        }
-
-        binding.backButton.setOnClickListener {
-            parentModel.backToLocation()
-        }
-
-        binding.setAndClaimButton.setOnClickListener {
-            parentModel.setFrequency(
-                model.getFrequency(binding.frequenciesSelector.selectedItemPosition)
-            )
-            parentModel.next()
-        }
+        binding.setFrequencyView.listener(
+            onFrequencyDocumentation = {
+                analytics.trackEventSelectContent(
+                    AnalyticsService.ParamValue.DOCUMENTATION_FREQUENCY.paramValue
+                )
+                navigator.openWebsite(context, it)
+            },
+            onBack = {
+                parentModel.backToLocation()
+            },
+            onSet = {
+                parentModel.setFrequency(
+                    model.getFrequency(it)
+                )
+                parentModel.next()
+            })
 
         model.onFrequencyState().observe(viewLifecycleOwner) { result ->
-            if (result.country.isNullOrEmpty()) {
-                binding.frequencySelectedText.visibility = View.GONE
-            } else {
-                binding.frequencySelectedText.text = getString(
-                    R.string.frequency_selected_text, result.country
-                )
-            }
-
-            binding.frequenciesSelector.adapter = ArrayAdapter(
-                requireContext(), android.R.layout.simple_spinner_dropdown_item, result.frequencies
-            )
+            binding.setFrequencyView.defaultState(result, true)
         }
     }
 }
