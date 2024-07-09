5089a1645d47714e4c8ae1bc2cd7ec45018d1c8a
Pavlos Tzegiannakis <pavlostzeg@gmail.com>
2024-07-09 15:50:11 +0300

    Fix scanning Pulse barcode by removing the "P" prefix and moving to next screen if successful

--- app/src/main/java/com/weatherxm/ui/claimdevice/pulse/preparegateway/ClaimPulsePrepareGatewayFragment.kt
+++ app/src/main/java/com/weatherxm/ui/claimdevice/pulse/preparegateway/ClaimPulsePrepareGatewayFragment.kt
@@ -46,10 +46,10 @@ class ClaimPulsePrepareGatewayFragment : BaseFragment() {
     private fun scanBarcode() {
         scanner.startScan()
             .addOnSuccessListener { barcode ->
-                val scannedInfo = barcode.rawValue ?: String.empty()
+                val scannedInfo = barcode.rawValue?.removePrefix("P") ?: String.empty()
                 if (model.validateSerial(scannedInfo)) {
                     model.setSerialNumber(scannedInfo)
-                    // TODO: Go to next page 
+                    model.next(2)
                 } else {
                     showSnackbarMessage(
                         binding.root, getString(R.string.prepare_gateway_invalid_barcode)
