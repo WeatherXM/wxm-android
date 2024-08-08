f22b4bfedbb7e2781b7123b2616ee4af36c61767
Pavlos Tzegiannakis <pavlostzeg@gmail.com>
2024-08-08 13:59:05 +0300

    Tests for DeviceOTA, Explorer, Widget and BluetoothConnection repositories

---
+++ app/src/test/java/com/weatherxm/data/repository/WidgetRepositoryTest.kt
@@ -0,0 +1,59 @@
+package com.weatherxm.data.repository
+
+import arrow.core.Either
+import com.weatherxm.TestConfig.failure
+import com.weatherxm.TestUtils.isSuccess
+import com.weatherxm.TestUtils.mockEitherLeft
+import com.weatherxm.TestUtils.mockEitherRight
+import com.weatherxm.data.datasource.WidgetDataSource
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.justRun
+import io.mockk.mockk
+import io.mockk.verify
+
+class WidgetRepositoryTest : BehaviorSpec({
+    val dataSource = mockk<WidgetDataSource>()
+    val repository = WidgetRepositoryImpl(dataSource)
+
+    val widgetId = 1
+    val deviceId = "deviceId"
+
+    beforeSpec {
+        justRun { dataSource.setWidgetId(widgetId) }
+        justRun { dataSource.removeWidgetId(widgetId) }
+        justRun { dataSource.setWidgetDevice(widgetId, deviceId) }
+    }
+
+    context("Perform Widget-related actions") {
+        given("A widget ID") {
+            then("The widget ID should be set") {
+                repository.setWidgetId(widgetId)
+                verify(exactly = 1) { dataSource.setWidgetId(widgetId) }
+            }
+            then("The widget ID should be removed") {
+                repository.removeWidgetId(widgetId)
+                verify(exactly = 1) { dataSource.removeWidgetId(widgetId) }
+            }
+            then("The device ID should be set") {
+                repository.setWidgetDevice(widgetId, deviceId)
+                verify(exactly = 1) { dataSource.setWidgetDevice(widgetId, deviceId) }
+            }
+            and("The device ID should be retrieved") {
+                When("is a success") {
+                    mockEitherRight({ dataSource.getWidgetDevice(widgetId) }, deviceId)
+                    then("The device ID should be returned") {
+                        repository.getWidgetDevice(widgetId).isSuccess(deviceId)
+                    }
+                }
+                When("is a failure") {
+                    mockEitherLeft({ dataSource.getWidgetDevice(widgetId) }, failure)
+                    then("The device ID should not be returned") {
+                        repository.getWidgetDevice(widgetId) shouldBe Either.Left(failure)
+                    }
+                }
+            }
+        }
+    }
+
+})
---
+++ app/src/test/java/com/weatherxm/data/repository/bluetooth/BluetoothConnectionRepositoryTest.kt
@@ -0,0 +1,124 @@
+package com.weatherxm.data.repository.bluetooth
+
+import android.bluetooth.BluetoothDevice
+import arrow.core.Either
+import com.weatherxm.TestConfig.failure
+import com.weatherxm.TestUtils.coMockEitherLeft
+import com.weatherxm.TestUtils.coMockEitherRight
+import com.weatherxm.TestUtils.isSuccess
+import com.weatherxm.data.Frequency
+import com.weatherxm.data.datasource.bluetooth.BluetoothConnectionDataSource
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.coEvery
+import io.mockk.coJustRun
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.flow.Flow
+
+class BluetoothConnectionRepositoryTest : BehaviorSpec({
+    lateinit var dataSource: BluetoothConnectionDataSource
+    lateinit var repository: BluetoothConnectionRepository
+
+    val pairedDevices = listOf<BluetoothDevice>(mockk())
+    val bondStatusFlow = mockk<Flow<Int>>()
+    val macAddress = "macAddress"
+    val frequency = Frequency.EU868
+    val testClaimingKey = "testClaimingKey"
+    val testDeviceEUI = "testDeviceEUI"
+
+    beforeContainer {
+        dataSource = mockk<BluetoothConnectionDataSource>()
+        repository = BluetoothConnectionRepositoryImpl(dataSource)
+        coEvery { dataSource.getPairedDevices() } returns pairedDevices
+        coEvery { dataSource.registerOnBondStatus() } returns bondStatusFlow
+        coJustRun { dataSource.disconnectFromPeripheral() }
+    }
+
+    given("Some paired devices") {
+        then("The repository should return them") {
+            repository.getPairedDevices() shouldBe pairedDevices
+        }
+    }
+
+    context("Get the Bond Status Flow") {
+        given("The data source providing the flow") {
+            then("The repository should return that flow") {
+                repository.registerOnBondStatus() shouldBe bondStatusFlow
+            }
+        }
+    }
+
+    context("Perform peripheral related actions") {
+        given("A data source exposing peripheral related actions") {
+            and("Set Peripheral") {
+                When("success") {
+                    coMockEitherRight({ dataSource.setPeripheral(macAddress) }, Unit)
+                    repository.setPeripheral(macAddress).isSuccess(Unit)
+                }
+                When("failure") {
+                    coMockEitherLeft({ dataSource.setPeripheral(macAddress) }, failure)
+                    repository.setPeripheral(macAddress) shouldBe Either.Left(failure)
+                }
+            }
+            and("Connect to Peripheral") {
+                When("success") {
+                    coMockEitherRight({ dataSource.connectToPeripheral() }, Unit)
+                    repository.connectToPeripheral().isSuccess(Unit)
+                }
+                When("failure") {
+                    coMockEitherLeft({ dataSource.connectToPeripheral() }, failure)
+                    repository.connectToPeripheral() shouldBe Either.Left(failure)
+                }
+            }
+            and("Disconnect from Peripheral") {
+                repository.disconnectFromPeripheral()
+                coVerify(exactly = 1) { dataSource.disconnectFromPeripheral() }
+            }
+            and("Fetch Claiming Key") {
+                When("success") {
+                    coMockEitherRight({ dataSource.fetchClaimingKey() }, testClaimingKey)
+                    repository.fetchClaimingKey().isSuccess(testClaimingKey)
+                }
+                When("failure") {
+                    coMockEitherLeft({ dataSource.fetchClaimingKey() }, failure)
+                    repository.fetchClaimingKey() shouldBe Either.Left(failure)
+                }
+            }
+            and("Fetch Device EUI") {
+                When("success") {
+                    coMockEitherRight({ dataSource.fetchDeviceEUI() }, testDeviceEUI)
+                    repository.fetchDeviceEUI().isSuccess(testDeviceEUI)
+                }
+                When("failure") {
+                    coMockEitherLeft({ dataSource.fetchClaimingKey() }, failure)
+                    repository.fetchClaimingKey() shouldBe Either.Left(failure)
+                }
+            }
+            and("Set Frequency") {
+                When("success") {
+                    coMockEitherRight({ dataSource.setFrequency(frequency) }, Unit)
+                    repository.setFrequency(frequency).isSuccess(Unit)
+                }
+                When("failure") {
+                    coMockEitherLeft({ dataSource.setFrequency(frequency) }, failure)
+                    repository.setFrequency(frequency) shouldBe Either.Left(failure)
+                }
+            }
+            and("Reboot") {
+                When("success") {
+                    coMockEitherRight({ dataSource.reboot() }, Unit)
+                    repository.reboot().isSuccess(Unit)
+                    then("Disconnect from the peripheral") {
+                        coVerify(exactly = 1) { dataSource.disconnectFromPeripheral() }
+                    }
+                }
+                When("failure") {
+                    coMockEitherLeft({ dataSource.reboot() }, failure)
+                    repository.reboot() shouldBe Either.Left(failure)
+                }
+            }
+
+        }
+    }
+})
