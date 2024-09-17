cbf63701a95ac6a3c8167234a5ad7f94f7fb8b45
Pavlos Tzegiannakis <pavlostzeg@gmail.com>
2024-09-17 14:48:14 +0300

    StartupUseCaseTest

---
+++ app/src/test/java/com/weatherxm/usecases/StartupUseCaseTest.kt
@@ -0,0 +1,78 @@
+package com.weatherxm.usecases
+
+import com.weatherxm.TestConfig.context
+import com.weatherxm.TestConfig.failure
+import com.weatherxm.TestUtils.coMockEitherLeft
+import com.weatherxm.TestUtils.coMockEitherRight
+import com.weatherxm.data.repository.AppConfigRepository
+import com.weatherxm.data.repository.AuthRepository
+import com.weatherxm.data.repository.UserPreferencesRepository
+import com.weatherxm.service.workers.RefreshFcmApiWorker
+import com.weatherxm.ui.startup.StartupState
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.types.shouldBeTypeOf
+import io.mockk.coJustRun
+import io.mockk.coVerify
+import io.mockk.every
+import io.mockk.justRun
+import io.mockk.mockk
+import io.mockk.mockkObject
+import kotlinx.coroutines.flow.first
+
+class StartupUseCaseTest : BehaviorSpec({
+    val authRepo = mockk<AuthRepository>()
+    val appConfigRepo = mockk<AppConfigRepository>()
+    val userPreferencesRepo = mockk<UserPreferencesRepository>()
+    val usecase = StartupUseCaseImpl(context, authRepo, userPreferencesRepo, appConfigRepo)
+
+    beforeSpec {
+        coJustRun { appConfigRepo.setLastRemindedVersion() }
+        mockkObject(RefreshFcmApiWorker)
+        justRun { RefreshFcmApiWorker.initAndRefreshToken(context, null) }
+    }
+
+    suspend fun getStartupType() = usecase.getStartupState().first()
+
+    context("Get startup state") {
+        given("Some repositories providing data regarding the startup state") {
+            When("app should be updated") {
+                every { appConfigRepo.shouldUpdate() } returns true
+                then("return ShowUpdate") {
+                    getStartupType().shouldBeTypeOf<StartupState.ShowUpdate>()
+                }
+                then("set the last reminded version") {
+                    coVerify(exactly = 1) { appConfigRepo.setLastRemindedVersion() }
+                }
+            }
+            When("app should not be updated") {
+                every { appConfigRepo.shouldUpdate() } returns false
+                and("user is logged in") {
+                    coMockEitherRight({ authRepo.isLoggedIn() }, true)
+                    and("we should show the opt-in analytics screen") {
+                        every { userPreferencesRepo.shouldShowAnalyticsOptIn() } returns true
+                        then("return ShowAnalyticsOptIn") {
+                            getStartupType().shouldBeTypeOf<StartupState.ShowAnalyticsOptIn>()
+                        }
+                    }
+                    and("we should show home") {
+                        every { userPreferencesRepo.shouldShowAnalyticsOptIn() } returns false
+                        then("return ShowHome") {
+                            getStartupType().shouldBeTypeOf<StartupState.ShowHome>()
+                        }
+                    }
+                    then("refresh the FCM token (exactly = 2 because we have 2 `and` conditions") {
+                        coVerify(exactly = 2) {
+                            RefreshFcmApiWorker.initAndRefreshToken(context, null)
+                        }
+                    }
+                }
+                and("User is logged out") {
+                    coMockEitherLeft({ authRepo.isLoggedIn() }, failure)
+                    then("return ShowExplorer") {
+                        getStartupType().shouldBeTypeOf<StartupState.ShowExplorer>()
+                    }
+                }
+            }
+        }
+    }
+})
