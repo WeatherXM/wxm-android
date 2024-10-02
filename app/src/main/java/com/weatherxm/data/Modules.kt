bc6f0f97b474127fa9b5a454863bc1b775c347f6
Pavlos Tzegiannakis <pavlostzeg@gmail.com>
2024-10-02 14:13:16 +0300

    Fix deprecated imports of Koin's viewModel (#345)

--- app/src/main/java/com/weatherxm/data/Modules.kt
+++ app/src/main/java/com/weatherxm/data/Modules.kt
@@ -257,7 +257,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import okhttp3.logging.HttpLoggingInterceptor.Level
 import org.koin.android.ext.koin.androidContext
-import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.module.dsl.viewModel
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import retrofit2.Retrofit
