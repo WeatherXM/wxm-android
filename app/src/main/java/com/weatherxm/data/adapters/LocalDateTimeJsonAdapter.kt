029a65c23adde4f0671bed64c7edd69f5a95be2d
Pavlos Tzegiannakis <pavlostzeg@gmail.com>
2024-10-14 13:53:17 +0300

    Add PoL Reasoning

--- app/src/main/java/com/weatherxm/data/adapters/LocalDateTimeJsonAdapter.kt
+++ app/src/main/java/com/weatherxm/data/adapters/LocalDateTimeJsonAdapter.kt
@@ -9,7 +9,8 @@ import java.time.format.DateTimeFormatter
 class LocalDateTimeJsonAdapter : JsonAdapter<LocalDateTime>() {
 
     override fun fromJson(reader: JsonReader): LocalDateTime? {
-        if (reader.peek() == null) {
+        val nextToken = reader.peek()
+        if (nextToken == null || nextToken == JsonReader.Token.NULL) {
             return reader.nextNull()
         }
         val string = reader.nextString()
