68dccc543de535cf986a515a3e14fce25627b8b7
Pavlos Tzegiannakis <pavlostzeg@gmail.com>
2024-10-24 15:26:43 +0300

    Finalize claiming unit tests

--- app/src/test/java/com/weatherxm/ui/claimdevice/helium/result/ClaimHeliumResultViewModelTest.kt
+++ app/src/test/java/com/weatherxm/ui/claimdevice/helium/result/ClaimHeliumResultViewModelTest.kt
@@ -2,11 +2,14 @@ package com.weatherxm.ui.claimdevice.helium.result
 
 import com.weatherxm.R
 import com.weatherxm.TestConfig.resources
+import com.weatherxm.TestUtils.coMockEitherLeft
+import com.weatherxm.TestUtils.coMockEitherRight
 import com.weatherxm.analytics.AnalyticsWrapper
 import com.weatherxm.data.models.BluetoothError
 import com.weatherxm.data.models.Frequency
 import com.weatherxm.ui.InstantExecutorListener
 import com.weatherxm.ui.common.ScannedDevice
+import com.weatherxm.ui.common.UIError
 import com.weatherxm.usecases.BluetoothConnectionUseCase
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
@@ -14,10 +17,14 @@ import io.kotest.matchers.shouldNotBe
 import io.mockk.every
 import io.mockk.justRun
 import io.mockk.mockk
+import io.mockk.verify
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
 
 @OptIn(ExperimentalCoroutinesApi::class)
@@ -26,14 +33,19 @@ class ClaimHeliumResultViewModelTest : BehaviorSpec({
     val analytics = mockk<AnalyticsWrapper>()
     lateinit var viewModel: ClaimHeliumResultViewModel
 
-    val scannedDevice = mockk<ScannedDevice>()
+    val scannedDevice = ScannedDevice.empty()
     val frequency = Frequency.EU868
     val devEui = "devEUI"
+    val devEuiWithColon = "dev:EUI"
     val claimingKey = "claimingKey"
+    val bondFlow = MutableSharedFlow<Int>(
+        replay = 1, onBufferOverflow = BufferOverflow.DROP_LATEST
+    )
 
     val bluetoothDisabledFailure = BluetoothError.BluetoothDisabledException()
     val connectionLostFailure = BluetoothError.ConnectionLostException()
     val connectionRejectedFailure = BluetoothError.ConnectionRejectedError()
+    val atCommandFailure = BluetoothError.ATCommandError()
     val bluetoothDisabled = "Bluetooth Disabled"
     val connectionLost = "Connection Lost"
     val connectionRejected = "Connection Rejected"
@@ -53,9 +65,17 @@ class ClaimHeliumResultViewModelTest : BehaviorSpec({
         every { resources.getString(R.string.set_frequency_failed_desc) } returns setFrequencyFailed
         every { resources.getString(R.string.helium_reboot_failed) } returns rebootFailed
         every { resources.getString(R.string.helium_fetching_info_failed) } returns fetchingInfoFailed
+        every { usecase.registerOnBondStatus() } returns bondFlow
 
         viewModel = ClaimHeliumResultViewModel(usecase, resources, analytics)
     }
+
+    fun UIError?.testBLEError(code: String?, message: String) {
+        this?.errorCode shouldBe code
+        this?.errorMessage shouldBe message
+        this?.retryFunction shouldNotBe null
+    }
+
 
     context("SET and then GET the scanned device") {
         given("a scanned device") {
@@ -76,22 +96,116 @@ class ClaimHeliumResultViewModelTest : BehaviorSpec({
             When("it's BluetoothDisabledException") {
                 viewModel.onConnectionFailure(bluetoothDisabledFailure)
                 then("onBLEError should post the respective error") {
-                    viewModel.onBLEError().value?.errorCode shouldBe bluetoothDisabledFailure.code
-                    viewModel.onBLEError().value?.errorMessage shouldBe bluetoothDisabled
-                    viewModel.onBLEError().value?.retryFunction shouldNotBe null
+                    viewModel.onBLEError().value.testBLEError(
+                        bluetoothDisabledFailure.code,
+                        bluetoothDisabled
+                    )
                 }
             }
             When("it's ConnectionLostException") {
                 viewModel.onConnectionFailure(connectionLostFailure)
-                viewModel.onBLEError().value?.errorCode shouldBe connectionLostFailure.code
-                viewModel.onBLEError().value?.errorMessage shouldBe connectionLost
-                viewModel.onBLEError().value?.retryFunction shouldNotBe null
+                viewModel.onBLEError().value.testBLEError(
+                    connectionLostFailure.code,
+                    connectionLost
+                )
             }
             When("it's any other Failure") {
                 viewModel.onConnectionFailure(connectionRejectedFailure)
-                viewModel.onBLEError().value?.errorCode shouldBe connectionRejectedFailure.code
-                viewModel.onBLEError().value?.errorMessage shouldBe connectionRejected
-                viewModel.onBLEError().value?.retryFunction shouldNotBe null
+                viewModel.onBLEError().value.testBLEError(
+                    connectionRejectedFailure.code,
+                    connectionRejected
+                )
+            }
+        }
+    }
+
+    context("Flow of setting the frequency") {
+        given("a usecase assisting with the flow starting with setting the frequency") {
+            When("it's a failure") {
+                coMockEitherLeft({ usecase.setFrequency(frequency) }, connectionLostFailure)
+                runTest { viewModel.setFrequency(frequency) }
+                then("LiveData onBLEError should post the respective error") {
+                    viewModel.onBLEError().value.testBLEError(
+                        connectionLostFailure.code,
+                        setFrequencyFailed
+                    )
+                }
+                then("track the event's failure in analytics") {
+                    verify(exactly = 1) { analytics.trackEventFailure(connectionLostFailure.code) }
+                }
+            }
+            When("it's a success start the reboot flow") {
+                coMockEitherRight({ usecase.setFrequency(frequency) }, Unit)
+                When("it's a failure") {
+                    coMockEitherLeft({ usecase.reboot() }, connectionRejectedFailure)
+                    runTest { viewModel.setFrequency(frequency) }
+                    then("LiveData onBLEError should post the respective error") {
+                        viewModel.onBLEError().value.testBLEError(
+                            connectionRejectedFailure.code,
+                            rebootFailed
+                        )
+                    }
+                    then("track the event's failure in analytics") {
+                        verify(exactly = 1) {
+                            analytics.trackEventFailure(connectionRejectedFailure.code)
+                        }
+                    }
+                }
+                When("it's a success start the connect flow") {
+                    coMockEitherRight({ usecase.reboot() }, Unit)
+                    runTest { viewModel.setFrequency(frequency) }
+                    then("LiveData onBLEConnection should post a true value") {
+                        viewModel.onBLEConnection().value shouldBe true
+                    }
+                }
+            }
+        }
+    }
+
+    context("Flow after we have connected to the device (fetch Dev EUI and claiming key)") {
+        given("a usecase providing the Dev EUI") {
+            When("it's a failure") {
+                coMockEitherLeft({ usecase.fetchDeviceEUI() }, bluetoothDisabledFailure)
+                runTest { viewModel.onConnected() }
+                then("LiveData onBLEError should post the respective error") {
+                    viewModel.onBLEError().value.testBLEError(
+                        bluetoothDisabledFailure.code,
+                        fetchingInfoFailed
+                    )
+                }
+                then("track the event's failure in analytics") {
+                    verify(exactly = 1) {
+                        analytics.trackEventFailure(bluetoothDisabledFailure.code)
+                    }
+                }
+            }
+            When("it's a success") {
+                coMockEitherRight({ usecase.fetchDeviceEUI() }, devEuiWithColon)
+                coMockEitherLeft({ usecase.fetchClaimingKey() }, atCommandFailure)
+                runTest { viewModel.onConnected() }
+                then("LiveData onBLEDevEUI should post the Dev EUI unmasked (without `:`)") {
+                    viewModel.onBLEDevEUI().value shouldBe devEui
+                }
+                When("fetching the claiming key is a failure") {
+                    then("LiveData onBLEError should post the respective error") {
+                        viewModel.onBLEError().value.testBLEError(
+                            atCommandFailure.code,
+                            fetchingInfoFailed
+                        )
+                    }
+                    then("track the event's failure in analytics") {
+                        verify(exactly = 1) {
+                            analytics.trackEventFailure(atCommandFailure.code)
+                        }
+                    }
+                }
+                When("fetching the claiming key is a success") {
+                    coMockEitherRight({ usecase.fetchClaimingKey() }, claimingKey)
+                    runTest { viewModel.onConnected() }
+                    then("LiveData onBLEClaimingKey should post the claiming key") {
+                        viewModel.onBLEClaimingKey().value shouldBe claimingKey
+                    }
+                }
             }
         }
     }
