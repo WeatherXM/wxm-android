9bb2b7305fbc4eaf4fcc9d785bb3a2082ae2da9e
Pavlos Tzegiannakis <pavlostzeg@gmail.com>
2024-12-05 15:58:45 +0200

    Save the captured photo in files, save their paths and update the UI based on the number of current photos

--- app/src/main/java/com/weatherxm/ui/photoverification/gallery/PhotoGalleryViewModel.kt
+++ app/src/main/java/com/weatherxm/ui/photoverification/gallery/PhotoGalleryViewModel.kt
@@ -1,5 +1,7 @@
 package com.weatherxm.ui.photoverification.gallery
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class PhotoGalleryViewModel(
@@ -7,4 +9,27 @@ class PhotoGalleryViewModel(
     val fromClaiming: Boolean
 ) : ViewModel() {
     private val newPhotosTaken = mutableListOf<String>()
+
+    private val onPhotoNumber = MutableLiveData(getPhotoNumber())
+
+    fun onPhotoNumber(): LiveData<Int> = onPhotoNumber
+
+    private fun getPhotoNumber() = newPhotosTaken.size + currentPhotosList.size
+
+    fun addPhoto(path: String) {
+        if (path.isNotEmpty() && !newPhotosTaken.contains(path)) {
+            newPhotosTaken.add(path)
+            onPhotoNumber.postValue(getPhotoNumber())
+        }
+    }
+
+    fun deletePhoto(path: String) {
+        if (currentPhotosList.contains(path)) {
+            // TODO: Call the delete endpoint and post the new number on success
+            onPhotoNumber.postValue(getPhotoNumber())
+        } else if (newPhotosTaken.contains(path)) {
+            newPhotosTaken.remove(path)
+            onPhotoNumber.postValue(getPhotoNumber())
+        }
+    }
 }
