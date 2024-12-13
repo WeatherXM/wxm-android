ccbd3454073641258cbf763dd0e17f0da2d66ed4
Pavlos Tzegiannakis <pavlostzeg@gmail.com>
2024-12-13 13:48:51 +0200

    Feature/fe 1344 photo capturing and gallery screens (#437)
    * [WIP] Implement the first states of the photo gallery. Without camera permission and with no photos.
    
    * Fix permissions for camera
    
    * Upload button functionality
    
    * Save the captured photo in files, save their paths and update the UI based on the number of current photos
    
    * Create the thumbnails. Moved normalizedName function to UIDevice. Pass device object to photo verification screens.
    
    * Support landscape photos. Show selected photo in full size.
    
    * Delete the local file when the user presses the delete button
    
    * Fix detekt
    
    * Fix unit tests
    
    * Open camera permisssions and camera only if gallery is empy. Auto-select the latest photo taken at all cases.
    
    * On gallery onCreate click the "+" button automatically. Fix background of good and bad examples in intro.
    
    * Share photos implementation (including test code) (#438)
    
    * Share photos implementation (including test code)
    
    * Use a different file provider for each flavor (distinct by applicationId)

---
+++ app/src/main/java/com/weatherxm/ui/photoverification/gallery/PhotoGalleryViewModel.kt
@@ -0,0 +1,61 @@
+package com.weatherxm.ui.photoverification.gallery
+
+import android.content.Context
+import android.net.Uri
+import androidx.compose.runtime.mutableStateListOf
+import androidx.core.content.FileProvider
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.weatherxm.ui.common.Contracts.FILE_PROVIDER_AUTHORITY
+import com.weatherxm.ui.common.StationPhoto
+import com.weatherxm.ui.common.UIDevice
+import java.io.File
+
+class PhotoGalleryViewModel(
+    val device: UIDevice,
+    val photos: MutableList<StationPhoto>,
+    val fromClaiming: Boolean
+) : ViewModel() {
+    private val onPhotosNumber = MutableLiveData(photos.size)
+    private val _onPhotos = mutableStateListOf<StationPhoto>().apply {
+        addAll(photos)
+    }
+    val onPhotos: List<StationPhoto> = _onPhotos
+
+    fun onPhotosNumber(): LiveData<Int> = onPhotosNumber
+
+    fun addPhoto(path: String) {
+        if (path.isNotEmpty() && photos.firstOrNull { it.localPath == path } == null) {
+            val stationPhoto = StationPhoto(null, path)
+            photos.add(stationPhoto)
+            _onPhotos.add(stationPhoto)
+            onPhotosNumber.postValue(photos.size)
+        }
+    }
+
+    fun deletePhoto(photo: StationPhoto) {
+        if (photos.firstOrNull { it.remotePath == photo.remotePath } != null) {
+            // TODO: Call the delete endpoint and post the new state on success
+            photos.remove(photo)
+            _onPhotos.remove(photo)
+            onPhotosNumber.postValue(photos.size)
+        } else if (photos.firstOrNull { it.localPath == photo.localPath } != null) {
+            photos.remove(photo)
+            _onPhotos.remove(photo)
+            onPhotosNumber.postValue(photos.size)
+        }
+    }
+
+    fun getUrisOfLocalPhotos(context: Context): ArrayList<Uri> {
+        val uris = arrayListOf<Uri>()
+        photos.forEach {
+            if (!it.localPath.isNullOrEmpty()) {
+                uris.add(
+                    FileProvider.getUriForFile(context, FILE_PROVIDER_AUTHORITY, File(it.localPath))
+                )
+            }
+        }
+        return uris
+    }
+}
