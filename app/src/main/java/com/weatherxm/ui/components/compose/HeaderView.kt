352892e8b15a85cc26b6dfd16b45008f3452337d
Pavlos Tzegiannakis <pavlostzeg@gmail.com>
2025-01-20 13:15:35 +0200

    Enable previews in new composables

--- app/src/main/java/com/weatherxm/ui/components/compose/HeaderView.kt
+++ app/src/main/java/com/weatherxm/ui/components/compose/HeaderView.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.weatherxm.R
 
@@ -41,4 +42,10 @@ fun HeaderView(
             MediumText(subtitle, colorRes = R.color.darkGrey)
         }
     }
+}
+
+@Preview
+@Composable
+fun PreviewHeaderView() {
+    HeaderView(title = "Title", subtitle = "Subtitle", onInfoButton = {})
 }
