ab73692afd19a0fc4c386a7e6ce414a4b26526b2
Pavlos Tzegiannakis <pavlostzeg@gmail.com>
2025-01-20 11:36:56 +0200

    Revert "Refactor RetryCardView as composable"
    This reverts commit ebb48269845c8161e30c5cabd29c4870bcc626b5.

--- app/src/main/java/com/weatherxm/ui/devicesrewards/DevicesRewardsActivity.kt
+++ app/src/main/java/com/weatherxm/ui/devicesrewards/DevicesRewardsActivity.kt
@@ -1,22 +1,6 @@
 package com.weatherxm.ui.devicesrewards
 
 import android.os.Bundle
-import androidx.compose.foundation.layout.Arrangement.spacedBy
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
 import com.weatherxm.R
 import com.weatherxm.analytics.AnalyticsService
 import com.weatherxm.databinding.ActivityDevicesRewardsBinding
@@ -28,7 +12,6 @@ import com.weatherxm.ui.common.invisible
 import com.weatherxm.ui.common.parcelable
 import com.weatherxm.ui.common.visible
 import com.weatherxm.ui.components.BaseActivity
-import com.weatherxm.ui.components.compose.LargeText
 import com.weatherxm.util.NumberUtils.formatTokens
 import com.weatherxm.util.initTotalEarnedChart
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -136,49 +119,11 @@ class DevicesRewardsActivity : BaseActivity() {
         binding.totalEarnedChart.invisible()
         binding.totalEarned.invisible()
         binding.totalEarnedStatus.visible(false)
-        binding.retryCard.setContent {
-            RetryCard {
-                model.getDevicesRewardsByRangeTotals(
-                    binding.totalEarnedRangeSelector.checkedChipId()
-                )
-            }
+        binding.retryCard.listener {
+            model.getDevicesRewardsByRangeTotals(
+                binding.totalEarnedRangeSelector.checkedChipId()
+            )
         }
         binding.retryCard.visible(true)
-    }
-}
-
-@Suppress("FunctionNaming")
-@Composable
-fun RetryCard(onClickListener: () -> Unit) {
-    Card(
-        colors = CardDefaults.cardColors(
-            containerColor = colorResource(R.color.colorSurface)
-        ),
-        onClick = { onClickListener() },
-        elevation = CardDefaults.cardElevation(0.dp)
-    ) {
-        Column(
-            Modifier
-                .padding(0.dp, dimensionResource(R.dimen.padding_normal_to_large))
-                .fillMaxWidth(),
-            verticalArrangement = spacedBy(dimensionResource(R.dimen.padding_small)),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Icon(
-                modifier = Modifier.padding(
-                    0.dp,
-                    0.dp,
-                    0.dp,
-                    dimensionResource(R.dimen.padding_small_to_normal)
-                ),
-                painter = painterResource(R.drawable.ic_retry),
-                contentDescription = null
-            )
-            LargeText(
-                text = stringResource(R.string.error_generic_message),
-                fontWeight = FontWeight.Bold
-            )
-            LargeText(text = stringResource(R.string.tap_to_retry))
-        }
     }
 }
