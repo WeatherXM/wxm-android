91176503e544396d223c3fd9a2091747f8b78644
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2025-04-14 09:27:05 +0300

    Removed unnecessary import

--- app/src/main/java/com/weatherxm/ui/photoverification/PhotoVerificationInstructionsFragment.kt
+++ app/src/main/java/com/weatherxm/ui/photoverification/PhotoVerificationInstructionsFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
