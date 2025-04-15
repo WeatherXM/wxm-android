cdf9cd8f0c6f5108eef5bf7063e68c7c7f92afb4
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2025-04-15 18:34:58 +0300

    Fixed PR comment

--- app/src/main/java/com/weatherxm/ui/photoverification/gallery/PhotoGalleryActivity.kt
+++ app/src/main/java/com/weatherxm/ui/photoverification/gallery/PhotoGalleryActivity.kt
@@ -98,22 +98,22 @@ class PhotoGalleryActivity : BaseActivity() {
         registerForActivityResult(
             ActivityResultContracts.PickVisualMedia()
         ) { uri ->
-            analytics.trackEventUserAction(
-                AnalyticsService.ParamValue.ADD_STATION_PHOTO.paramValue,
-                null,
-                customParams = arrayOf(
-                    Pair(
-                        AnalyticsService.CustomParam.ACTION.paramName,
-                        AnalyticsService.ParamValue.COMPLETED.paramValue
-                    ),
-                    Pair(
-                        AnalyticsService.CustomParam.SOURCE.paramName,
-                        AnalyticsService.ParamValue.GALLERY.paramValue
+            uri?.let {
+                analytics.trackEventUserAction(
+                    AnalyticsService.ParamValue.ADD_STATION_PHOTO.paramValue,
+                    null,
+                    customParams = arrayOf(
+                        Pair(
+                            AnalyticsService.CustomParam.ACTION.paramName,
+                            AnalyticsService.ParamValue.COMPLETED.paramValue
+                        ),
+                        Pair(
+                            AnalyticsService.CustomParam.SOURCE.paramName,
+                            AnalyticsService.ParamValue.GALLERY.paramValue
+                        )
                     )
                 )
-            )
 
-            uri?.let {
                 val file = createPhotoFile()
                 try {
                     contentResolver.openInputStream(it)?.use { inputStream ->
