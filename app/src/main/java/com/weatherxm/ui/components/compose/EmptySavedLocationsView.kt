25fb923fc3692d28bf478f4b69e4d1bf03091185
Pavlos Tzegiannakis <pavlostzeg@gmail.com>
2025-07-23 13:09:36 +0300

    Start the UI of the new home tab. Create the card for locations permission and the empty state on saved locations.

---
+++ app/src/main/java/com/weatherxm/ui/components/compose/EmptySavedLocationsView.kt
@@ -0,0 +1,74 @@
+package com.weatherxm.ui.components.compose
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.weatherxm.R
+
+@Suppress("FunctionNaming", "MagicNumber")
+@Preview
+@Composable
+fun EmptySavedLocationsView() {
+    val borderColor = colorResource(R.color.colorSurface)
+    val stroke = Stroke(
+        width = 3f,
+        pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f)
+    )
+    Box(
+        Modifier
+            .background(colorResource(R.color.colorBackground))
+            .fillMaxWidth()
+            .drawBehind {
+                drawRoundRect(
+                    color = borderColor,
+                    style = stroke,
+                    cornerRadius = CornerRadius(20.dp.toPx())
+                )
+            },
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier.padding(
+                horizontal = dimensionResource(R.dimen.padding_large),
+                vertical = dimensionResource(R.dimen.padding_normal)
+            ),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.margin_small))
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.ic_star_filled),
+                tint = colorResource(R.color.colorPrimary),
+                contentDescription = stringResource(R.string.action_close)
+            )
+            MediumText(stringResource(R.string.nothing_saved_yet))
+            Text(
+                text = stringResource(R.string.add_places_you_care),
+                color = colorResource(R.color.darkGrey),
+                style = MaterialTheme.typography.bodySmall,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+
+}
