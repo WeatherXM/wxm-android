ab1dc3ef3f1342a09b5a618f8b311b242b9a0640
Pavlos Tzegiannakis <pavlostzeg@gmail.com>
2025-09-02 14:36:31 +0300

    Hide the quests tab when in non-solana.

--- app/src/main/java/com/weatherxm/util/AndroidBuildInfo.kt
+++ app/src/main/java/com/weatherxm/util/AndroidBuildInfo.kt
@@ -1,10 +1,20 @@
 package com.weatherxm.util
 
 import android.os.Build
+import com.weatherxm.BuildConfig
 
 object AndroidBuildInfo {
     val sdkInt: Int = Build.VERSION.SDK_INT
     val release: String? = Build.VERSION.RELEASE
     val manufacturer: String? = Build.MANUFACTURER
     val model: String? = Build.MODEL
+
+    fun isSolana(): Boolean {
+        /**
+         * False warning by Android Studio on the APPLICATION_ID check when in non-Solana flavors.
+         * It will be true when the Solana flavor is used.
+         */
+        return manufacturer?.contains("Solana", true) == true ||
+            BuildConfig.APPLICATION_ID == "com.weatherxm.app.solana"
+    }
 }
