8770b2eccbff663bdd6a09441a764467b82d725b
Pavlos Tzegiannakis <pavlostzeg@gmail.com>
2025-11-13 17:35:46 +0200

    Manage subscription + plans screen

--- app/src/main/java/com/weatherxm/ui/managesubscription/ManageSubscriptionActivity.kt
+++ app/src/main/java/com/weatherxm/ui/managesubscription/ManageSubscriptionActivity.kt
@@ -1,29 +1,6 @@
 package com.weatherxm.ui.managesubscription
 
 import android.os.Bundle
-import androidx.compose.foundation.layout.Arrangement.spacedBy
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.weatherxm.R
 import com.weatherxm.analytics.AnalyticsService
 import com.weatherxm.databinding.ActivityManageSubscriptionBinding
 import com.weatherxm.service.BillingService
@@ -31,8 +8,6 @@ import com.weatherxm.ui.common.Contracts.ARG_HAS_FREE_TRIAL_AVAILABLE
 import com.weatherxm.ui.common.classSimpleName
 import com.weatherxm.ui.common.visible
 import com.weatherxm.ui.components.BaseActivity
-import com.weatherxm.ui.components.compose.LargeText
-import com.weatherxm.ui.components.compose.MediumText
 import org.koin.android.ext.android.inject
 
 class ManageSubscriptionActivity : BaseActivity() {
@@ -52,111 +27,32 @@ class ManageSubscriptionActivity : BaseActivity() {
             setNavigationOnClickListener { onBackPressedDispatcher.onBackPressed() }
         }
 
+        binding.currentPlanComposable.setContent {
+            CurrentPlanView(billingService.getActiveSub())
+        }
+
         binding.premiumFeaturesComposable.setContent {
-            PremiumFeatures {
-                // TODO: Show plans
+            PremiumFeaturesView {
+                binding.selectPlanComposable.visible(true)
+                binding.premiumFeaturesComposable.visible(false)
+                binding.currentPlanComposable.visible(false)
             }
         }
+
+        binding.selectPlanComposable.setContent {
+            PlansView(
+                plans = billingService.getAvailableSubs(hasFreeTrialAvailable)
+            ) {
+                // TODO: STOPSHIP: Start the billing flow
+            }
+        }
+
+        binding.currentPlanComposable.visible(true)
+        binding.premiumFeaturesComposable.visible(!billingService.hasActiveSub())
     }
 
     override fun onResume() {
         super.onResume()
         analytics.trackScreen(AnalyticsService.Screen.MANAGE_SUBSCRIPTION, classSimpleName())
-        binding.premiumFeaturesComposable.visible(!billingService.hasActiveSub())
-    }
-
-    @Suppress("FunctionNaming")
-    @Preview
-    @Composable
-    fun PremiumFeatures(onGetPremium: () -> Unit = {}) {
-        Column(
-            verticalArrangement = spacedBy(dimensionResource(R.dimen.margin_normal))
-        ) {
-            LargeText(
-                text = getString(R.string.premium_features),
-                fontWeight = FontWeight.Bold,
-                fontSize = 18.sp
-            )
-            Card(
-                colors = CardDefaults.cardColors(
-                    containerColor = colorResource(R.color.layer1)
-                ),
-                shape = RoundedCornerShape(dimensionResource(R.dimen.radius_small)),
-                elevation = CardDefaults.cardElevation(
-                    dimensionResource(R.dimen.elevation_small)
-                )
-            ) {
-                Column(
-                    modifier = Modifier.padding(dimensionResource(R.dimen.padding_normal)),
-                    verticalArrangement = spacedBy(dimensionResource(R.dimen.margin_normal))
-                ) {
-                    Row(
-                        horizontalArrangement = spacedBy(dimensionResource(R.dimen.margin_small_to_normal))
-                    ) {
-                        Icon(
-                            painter = painterResource(R.drawable.ic_checkmark_only),
-                            tint = colorResource(R.color.colorOnSurface),
-                            modifier = Modifier
-                                .padding(top = dimensionResource(R.dimen.padding_extra_small))
-                                .size(16.dp),
-                            contentDescription = null
-                        )
-                        Column(
-                            verticalArrangement = spacedBy(dimensionResource(R.dimen.margin_extra_small))
-                        ) {
-                            LargeText(
-                                text = getString(R.string.mosaic_forecast),
-                                fontWeight = FontWeight.Bold,
-                                fontSize = 18.sp
-                            )
-                            MediumText(
-                                text = getString(R.string.mosaic_forecast_explanation),
-                                colorRes = R.color.darkGrey
-                            )
-                        }
-                    }
-                    Row(
-                        horizontalArrangement = spacedBy(dimensionResource(R.dimen.margin_small_to_normal))
-                    ) {
-                        Icon(
-                            painter = painterResource(R.drawable.ic_checkmark_only),
-                            tint = colorResource(R.color.colorOnSurface),
-                            modifier = Modifier
-                                .padding(top = dimensionResource(R.dimen.padding_extra_small))
-                                .size(16.dp),
-                            contentDescription = null
-                        )
-                        Column(
-                            verticalArrangement = spacedBy(dimensionResource(R.dimen.margin_extra_small))
-                        ) {
-                            LargeText(
-                                text = getString(R.string.hourly_forecast),
-                                fontWeight = FontWeight.Bold,
-                                fontSize = 18.sp
-                            )
-                            MediumText(
-                                text = getString(R.string.just_the_basics),
-                                colorRes = R.color.darkGrey
-                            )
-                        }
-                    }
-                    Button(
-                        modifier = Modifier.fillMaxWidth(),
-                        onClick = onGetPremium,
-                        colors = ButtonDefaults.buttonColors(
-                            containerColor = colorResource(R.color.colorPrimary),
-                            contentColor = colorResource(R.color.colorBackground)
-                        ),
-                        shape = RoundedCornerShape(dimensionResource(R.dimen.radius_medium)),
-                    ) {
-                        MediumText(
-                            stringResource(R.string.get_premium),
-                            fontWeight = FontWeight.Bold,
-                            colorRes = R.color.colorBackground
-                        )
-                    }
-                }
-            }
-        }
     }
 }
