6f77f8582a8724965b24e8b67f6d7825b990333c
Pavlos Tzegiannakis <pavlostze@gmail.com>
2026-02-19 22:47:51 +0200

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,7 +89,7 @@ android {
         applicationId = "com.weatherxm.app"
         minSdk = 28
         targetSdk = 36
-        versionCode = 41 + getVersionGitTags(isSolana = false).size
+        versionCode = 42 + getVersionGitTags(isSolana = false).size
         versionName = getLastVersionGitTag(false, skipTagsLogging)
 
         androidResources {
