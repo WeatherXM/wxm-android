6761a74c22b6a008f5ead5fd488beb746c613853
eeVoskos <eeVoskos@gmail.com>
2026-02-22 23:33:21 +0200

    feat: Update app version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,7 +89,7 @@ android {
         applicationId = "com.weatherxm.app"
         minSdk = 28
         targetSdk = 36
-        versionCode = 43 + getVersionGitTags(isSolana = false).size
+        versionCode = 44 + getVersionGitTags(isSolana = false).size
         versionName = getLastVersionGitTag(false, skipTagsLogging)
 
         androidResources {
